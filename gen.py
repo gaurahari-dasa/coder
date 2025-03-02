0f983c809845c8389849eda3bd0144717e3c4842
Gaurahari Dasa <ganesh.hegde@hkm-group.org>
2025-03-02 16:15:27 +0530

    UI section, Haribol

--- gen.py
+++ gen.py
@@ -1,13 +1,78 @@
-import re;
+import re
+import io
 
 class UiSection:
-    lines = []
-
     def __init__(self):
-        pass
+        self.lines = []
+        self.output = io.StringIO()
 
     def append(self, line):
         self.lines.append(line)
+
+    def generate_text_input(self, field_name, title):
+        print(f'''<FormInput class="mt-4" id="{field_name}" title="{title}"
+            v-model="{self.form_type}.{field_name}" :error="{self.form_type}.errors.{field_name}" />''',
+            file=self.output)
+
+    def generate_select_input(self, field_name, title, options):
+        print(f'''<FormSelect class="mt-4" id="{field_name}" title="{title}" :options="{options}"
+            v-model="{self.form_type}.{field_name}" :error="{self.form_type}.errors.{field_name}" />''',
+            file=self.output)
+        
+    def generate_checkbox_input(self, field_name, title):
+        print(f'''<FormCheckBox class="mt-4" id="{field_name}" title="{title}" v-model="{self.form_type}.{field_name}" />''',
+            file=self.output)
+
+    def generate_control(self, field_name, ctrl_type, title, extra):
+        if ctrl_type == 'text':
+            self.generate_text_input(field_name, title)
+        elif ctrl_type == 'select':
+            self.generate_select_input(field_name, title, extra)
+        elif ctrl_type == 'checkbox':
+            self.generate_checkbox_input(field_name, title)
+        else:
+            print('Unknown control, Haribol!', field_name, ctrl_type)
+
+    def generate_form(self, form_type):
+        print(f'*** UI: {form_type} ***', file=self.output)
+        self.form_type = form_type
+        for line in self.lines:
+            m = re.match('([^,]+)[ ]*:[ ]*([^,]+)[ ]*,[ ]*([^,]+)(?:[ ]*,[ ]*(.*))?', line)
+            if m:
+                if '_' in m.group(1):
+                    ans = input('A field name has an underscore. Is this a mistake (enter \'y\' to bail out if it\'s a mistake)? ')
+                    if ans.lower() == 'y':
+                        return None
+                self.generate_control(m.group(1), m.group(2), m.group(3), m.group(4))
+        print('******\n', file=self.output)
+        return self.output
+
+    def generate(self):
+        if not self.generate_form('addForm'):
+            return None
+            
+        if not self.generate_form('editForm'):
+            return None
+        return self.output
+
+
+class ModelSection:
+    def __init__(self):
+        self.lines = []
+
+    def append(self, line):
+        self.lines.append(line)
+
+
+class SelectDataSection:
+    def __init__(self):
+        self.lines = []
+
+    def append(self, line):
+        self.lines.append(line)
+
+    def generate(self):
+        pass
 
 
 sections = []
@@ -21,60 +86,18 @@ def read_sections():
         if matched and matched.group(1) == 'UI':
             sections.append(UiSection())
             cur_sect = sections[-1]
+        elif matched and matched.group(1) == 'SelectData':
+            sections.append(SelectDataSection())
+            cur_sect = sections[-1]
         elif matched:
             print('section: <', matched.group(1), '>', sep='')
         elif cur_sect:
             cur_sect.append(line.strip())
 
+
 read_sections()
 
-form_type = None
 output = open('output.txt', 'wt',)
-
-def generate_text_input(field_name, title):
-    print(f'''
-        <FormInput class="mt-4" id="{field_name}" title="{title}"
-        v-model="{form_type}.{field_name}" :error="{form_type}.errors.{field_name}" />
-          ''', file=output)
-
-def generate_select_input(field_name, title, options):
-    print(f'''
-        <FormSelect class="mt-4" id="{field_name}" title="{title}" :options="{options}"
-            v-model="{form_type}.{field_name}" :error="{form_type}.errors.{field_name}" />
-          ''', file=output)
-    
-def generate_checkbox_input(field_name, title):
-    print(f'''
-        <FormCheckBox class="mt-4" id="{field_name}" title="{title}" v-model="{form_type}.{field_name}" />
-          ''', file=output)
-
-def generate_ui_control(field_name, ctrl_type, title, extra):
-    if ctrl_type == 'text':
-        generate_text_input(field_name, title)
-    elif ctrl_type == 'select':
-        generate_select_input(field_name, title, extra)
-    elif ctrl_type == 'checkbox':
-        generate_checkbox_input(field_name, title)
-    else:
-        print('Unknown control, Haribol!', field_name, ctrl_type)
-
-def generate_add_ui():
-    print('Generating UI for addForm ..')
-    global form_type
-    form_type = 'addForm'
-    for line in sections[0].lines:
-        m = re.match('([^,]+)[ ]*:[ ]*([^,]+)[ ]*,[ ]*([^,]+)(?:[ ]*,[ ]*(.*))?', line)
-        if (m):
-            generate_ui_control(m.group(1), m.group(2), m.group(3), m.group(4))
-
-def generate_edit_ui():
-    print('Generating UI for editForm ..')
-    global form_type
-    form_type = 'editForm'
-    for line in sections[0].lines:
-        m = re.match('([^,]+)[ ]*:[ ]*([^,]+)[ ]*,[ ]*([^,]+)(?:[ ]*,[ ]*(.*))?', line)
-        if (m):
-            generate_ui_control(m.group(1), m.group(2), m.group(3), m.group(4))
-
-generate_add_ui()
-generate_edit_ui()
+for section in sections:
+    if (gen := section.generate()):
+        output.write(gen.getvalue())
