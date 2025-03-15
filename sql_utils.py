4a90519b046e814ffe48518c9e7da9e362cd0765
Gaurahari Dasa <ganesh.hegde@hkm-group.org>
2025-03-15 11:00:38 +0530

    Connect to the DB only once, Haribol

--- sql_utils.py
+++ sql_utils.py
@@ -1,31 +1,32 @@
 import mysql.connector
-from utils import *
+import utils
+
+
+cnx = mysql.connector.connect(
+    user="root", password="", host="127.0.0.1", database="krishna_life"
+)
+
+if not (cnx and cnx.is_connected()):
+    utils.error("DB connection failure, Haribol!")
 
 
 def query(qry: str):
-    cnx = mysql.connector.connect(
-        user="root", password="", host="127.0.0.1", database="krishna_life"
-    )
-
-    if cnx and cnx.is_connected():
-        try:
-            with cnx.cursor() as cursor:
-                cursor.execute(qry)
-                cursor.fetchone()
-                return True
-        except:
-            return False
-        finally:
-            cnx.close()
-    else:
-        raise Exception("Failed to connect to the DB, Haribol!")
+    try:
+        with cnx.cursor() as cursor:
+            cursor.execute(qry)
+            cursor.fetchone()
+            return True
+    except:
+        return False
 
 
 def check_table(name):
-    if not query(f"SELECT * from {name} LIMIT 1"):
-        error(f"Table {name} seems to be missing, Haribol.")
+    check_column(name, "*")
 
 
 def check_column(table, column):
     if not query(f"SELECT {column} from {table} LIMIT 1"):
-        error(f"Column {column} in {table} seems to be missing, Haribol.")
+        if column == "*":
+            utils.error(f"Table {table} seems to be missing, Haribol.")
+        else:
+            utils.error(f"Column {column} in {table} seems to be missing, Haribol.")
