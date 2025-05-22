344a2729aa5d85494ca666207ff1d66c95577de1
gaurahari-dasa <ganesh.hegde@hkm-group.org>
2025-05-22 18:31:41 +0530

    Added vite and vue, Haribol

---
+++ web-app/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import FormInput from './components/FormInput.vue';
+</script>
+
+<template>
+  <div class="container p-4">
+    <h3 class="font-bold text-lg">Model Definition</h3>
+    <div class="grid grid-cols-4 gap-4">
+      <FormInput caption="Model Class" id="model" />
+      <FormInput caption="Context Model Class" id="ctxt-model" />
+    </div>
+  </div>
+</template>
---
+++ web-app/src/components/FormInput.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <label :for="id" class="block text-sm/6 font-medium text-gray-900">{{ caption }}</label>
+        <div class="mt-2">
+            <input
+                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                type="text" :id />
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps(['id', 'caption'])
+</script>
